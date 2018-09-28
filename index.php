55e887bc6d871c76f5edca250d4074f60e2900f2
poojit143 <jmotar.29@gmail.com>
2018-09-28 08:15:56 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AWS S3</title>
+</head>
+<body>
+  <form method="post" action="upload.php" enctype="multipart/form-data">
+  	<input type="file" name="fileToUpload">
+  	<input type="submit" name="sub">
+  </form>
+</body>
+</html>
